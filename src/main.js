2bbaa345b53d22fccedcdb8a4864d4743f2539b8
GEPANG <2455124772@qq.com>
2020-02-13 00:21:34 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
 //fastClick使用
 fastClick.attach(document.body)
+
+//使用轮播插件
+Vue.use(VueAwesomeSwiper)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,13 +1,19 @@
 <template>
     <div id="Home">
-        <div>home</div>
+        <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+
 export default {
-    name:"Home"
+    name:"Home",
+    components:{
+        HomeHeader,
+        HomeSwiper
+    }
 }
 </script>
-
-<style scoped></style>

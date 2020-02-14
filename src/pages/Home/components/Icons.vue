94c74f38f2d4144105fa2e15d23e54af36dc9840
GEPANG <2455124772@qq.com>
2020-02-14 22:20:43 +0800

    add recommend and wenkend

--- src/pages/Home/components/Icons.vue
+++ src/pages/Home/components/Icons.vue
@@ -98,37 +98,40 @@ export default {
     height 0
     padding-bottom 50%
 }
-.icon{
-    position relative
-    float left 
-    overflow hidden
-    width 25%
-    height 0
-    padding-bottom 25%
-    .icon-img{
-        position absolute
-        left  0
-        top 0
-        right 0
-        bottom .44rem
-        box-sizing border-box
-        padding .1rem
-        .icon-img-content{
-            height 100%
-            display block
-            margin 0 auto  
+#icons{
+    margin-top .1rem
+    .icon{
+        position relative
+        float left 
+        overflow hidden
+        width 25%
+        height 0
+        padding-bottom 25%
+        .icon-img{
+            position absolute
+            left  0
+            top 0
+            right 0
+            bottom .44rem
+            box-sizing border-box
+            padding .1rem
+            .icon-img-content{
+                height 100%
+                display block
+                margin 0 auto  
+            }
         }
-    }
-    .icon-desc{
-        position absolute
-        bottom 0
-        left 0
-        right 0
-        // height .44rem
-        line-height .44rem
-        text-align center
-        color $darktext
-        ellipsis()    
+        .icon-desc{
+            position absolute
+            bottom 0
+            left 0
+            right 0
+            // height .44rem
+            line-height .44rem
+            text-align center
+            color $darktext
+            ellipsis()    
+        }
     }
 }
 

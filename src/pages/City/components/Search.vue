78aa403be5e1b8b8f38bc280dd281631b42773d7
GEPANG <2455124772@qq.com>
2020-02-17 22:02:36 +0800

    add search to city

---
+++ src/pages/City/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="search">
+        <input type="text" class="search-input" placeholder="输入城市名或拼音">
+    </div>
+</template>
+<script>
+export default {
+    name:"citysearch"
+}
+</script>
+<style lang="stylus" scoped>
+@import '~@/assets/styles/varibles.styl'
+.search{
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input{
+        box-sizing border-box
+        width 100%
+        height .62rem
+        line-height .62rem
+        padding 0 .1rem
+        border-radius 0.06rem
+        text-align center
+        color #666
+    }
+}
+</style>

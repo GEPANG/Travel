ec7d956183387e2c4c23f279d0a3e16bd3534133
GEPANG <2455124772@qq.com>
2020-02-19 22:35:46 +0800

    search finish

--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -1,11 +1,66 @@
 <template>
-    <div class="search">
-        <input type="text" class="search-input" placeholder="输入城市名或拼音">
-    </div>
+   <div>
+        <div class="search">
+            <input v-model="keyword" type="text" class="search-input" placeholder="输入城市名或拼音">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item" 
+                    v-for="item of list" :key="item.id"
+                >
+                    {{item.name}}
+                </li>
+                <li class="search-item" v-show="hasNoData">没有匹配到数据</li>
+            </ul>
+        </div>
+   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-    name:"citysearch"
+    name:"citysearch",
+    props:{
+        cities:Object
+    },
+    data(){
+        return {
+            keyword:'',
+            list:[],
+            timer:null
+        }
+    },
+    computed:{
+        hasNoData(){
+            return !this.list.length
+        }
+    },
+    watch: {
+        keyword (){
+            if(this.timer){
+                clearTimeout(this.timer)
+            }
+            if(!this.keyword){
+                this.list=[]
+                return
+            }
+            this.timer=setTimeout(() => {
+                const result=[]
+                for (let i in this.cities){
+                    this.cities[i].forEach(value=>{
+                        if(value.spell.indexOf(this.keyword)>-1 ||
+                            value.name.indexOf(this.keyword)>-1
+                        ){
+                            result.push(value)
+                        }
+                    })
+                }  
+                this.list=result                  
+            }, 100);            
+        }
+    },
+    mounted(){
+        this.scroll=new Bscroll(this.$refs.search)
+    }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,6 +78,23 @@ export default {
         border-radius 0.06rem
         text-align center
         color #666
+    }
+}
+.search-content{
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    bottom 0
+    right 0
+    z-index 1
+    background #eee
+    .search-item{
+        line-height .62rem
+        padding-left .2rem
+        color #666
+        background #fff
+        border-bottom .02rem solid #ccc
     }
 }
 </style>

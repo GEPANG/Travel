156ee8af9436027b8b5cabe87a21c395cf91bf06
GEPANG <2455124772@qq.com>
2020-02-21 21:53:15 +0800

    header-animation

--- src/pages/Detail/detail.vue
+++ src/pages/Detail/detail.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="Detail">
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div style="height:2000px"></div>
     </div>
 </template>
 <script>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/Header'
 export default {
     name:"Detail",
     components:{
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 }
 </script>
